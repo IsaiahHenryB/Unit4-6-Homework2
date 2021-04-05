495b22bb94d5100cd538cc18c418284f22935bb1
IsaiahHenry <isaiahhenryb@gmail.com>
2021-04-05 13:43:55 -0400

    part 1 complete

--- HW-unit4-6-p2/index.js
+++ HW-unit4-6-p2/index.js
@@ -2,44 +2,67 @@
 
 //Using what you did with Objects perdiod 2 from the slides, create a personal profile. Instead of Products you will need to create a personal profile. Initialize Constructors and More!
 //Step 1
-
+function Profile(name,age,career,user){
+    this.name = name;
+    this.age = age;
+    this.career = career;
+    this.user = user;
+}
 
 
 
 //Step 2
 
-
+let profile1 = new Profile('Isaiah',"21","Producer");
 
 
 //Step 3
-
+let profile2 = new Profile('Debra',"57","Lunch Lady");
+let profile3 = new Profile('Jake',"38","State Farm");
+console.log(profile1,profile2,profile3)
 
 
 //Step 4
-
+function Admin(yearsActive,isAdmin){
+    this.yearsActive = yearsActive;
+    this.isAdmin = isAdmin;
+}
 
 //Step 5
-
+let user1 = new Admin(5,true)
+let user2 = new Admin(3,false)
+let user3 = new Admin(1,false)
 
 //Step 6
-
+profile1 = new Profile('Isaiah',"21","Producer",user1);
+profile2 = new Profile('Debra',"57","Lunch Lady",user2);
+profile3 = new Profile('Jake',"38","State Farm",user3);
+console.log(profile1,profile2,profile3)
 
 // Step 7
-
-
+user1.seniorMember = true
+console.log(profile1)
 // Step 8
 
 
 //.Create
 
 //Step 1
-
+let User = {
+    type: "Admin",
+    displayType:function(){
+        console.log(this.type)
+    }
+}
 
 //Step 2
-
+let newUser = Object.create(User);
+newUser.displayType();
 
 //Step 3
-
+let oldUser = Object.create(User);
+oldUser.type = "User";
+oldUser.displayType()
 
 
 //Array vs Objects
