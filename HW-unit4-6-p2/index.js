95b07da10b4a5548ef9435e18736ca5645e95ae8
IsaiahHenry <isaiahhenryb@gmail.com>
2021-04-06 16:45:25 -0400

    completed part2

--- HW-unit4-6-p2/index.js
+++ HW-unit4-6-p2/index.js
@@ -67,50 +67,74 @@ oldUser.displayType()
 
 //Array vs Objects
 
-//Objects: Create a hobby object with properties with name, descriptions and boolean of a property name of supplies needed.
+//Objects: Create a person object with properties with name, favorite number and boolean.
+let person = {
+    name: "Forte",
+    favNum: 76,
+    likesCoding: true,
 
-//Use dot notation to return hobby description
+}
 
+//Use dot notation to return favorite number
+console.log(person.name)
 //Use bracket notation to return name
+console.log(person["name"])
 
-
-//Arrays: Create an array with 4 different hobbies
-
-
-//access second item in the array
-
+//Arrays: Create an array with 4 different flavors
+const snappleFlavors = ['strawberry','kiwi','apple', 'fruit punch']
+console.log(person)
+//access third item in the array
+console.log(snappleFlavors[2])
 //Objects
 //Change the value of the boolean of the object we created.
-
-//Add a new property to our person object such as supplies with multiple values in the property.
-
-//remove the hobby description property from the person object
-
+person.likesCoding = false;
+console.log(person.likesCoding)
+//Add a new property to our person object such as hobbies with multiple values in the property.
+person.hobbies = ["biking","eating"];
+console.log(person.hobbies);
+//remove the favorite number property from the person object
+delete person.favNum;
+console.log(person);
 
 //Arrays
-//Use push to add another hobby to your array of hobbies
+//Use push to add another flavor to your array of flavors
+snappleFlavors.push("citrus blend");
+console.log(snappleFlavors);
+//use pop to remove the last item from an flavor array
+snappleFlavors.pop();
+console.log(snappleFlavors);
 
 
-//use pop to remove the last item from an hobby array
+//use unshift to add more values to the beginning of the flavor array.
+snappleFlavors.unshift('Sweet Tea','Lemon');
+console.log(snappleFlavors);
 
-
-//use unshift to add more values to the beginning of the hobby array.
-
-// us shift to remove the item from the hobby of the array
+// us shift to remove the item from the flavor of the array
+snappleFlavors.shift();
+console.log(snappleFlavors);
 
 //Object
 //Write a for in loop for the person object you created
-
+for (let name in person){
+    console.log(person.name)
+}
 //Write a for each loop for your person Object
-
-
+Object.keys(person).forEach(function(key) {
+    console.log(key); // logs keys in person
+   console.log(person[key]); // logs values in person
+   });
+   
 //Arrays
-//Write a for loop for your hobby array
+//Write a for loop for your flavors array
 
-//Write a for of loop for your hobby array
-
-//write a for each loop for your hobby array
-
-
-
+//Write a for of loop for your flavors array
+for(char of snappleFlavors){
+    console.log(char)
+}
+//write a for each loop for your flavors array
+snappleFlavors.forEach(item => {
+    console.log(item);
+    console.log(item.length)
+}
+)
 
